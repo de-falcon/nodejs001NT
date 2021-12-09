a55ca64bd3f5ad2f843860795c8c56c721b550c9
de-falcon <obidovhojiakbar7@gmail.com>
2021-12-09 16:43:09 +0500

    hided

---
+++ main.js
@@ -0,0 +1,9 @@
+let arr = [4,5,6,7,0,1,2,3]
+let str = "codeleet"
+let result = []
+for(let i = 0;i<arr.length;i++){
+    console.log('-----',arr[i],'-----');
+    for(let x = 0; x < str.length;x++){
+        console.log(str[x]);
+    }
+ }
